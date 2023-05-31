8bada27c22a78845d8977cb39af03aa4362a7103
Hisham Al-Harrasi <harrasi369@gmail.com>
2023-06-01 00:41:29 +0400

    chore(server): init spring project

---
+++ src/main/java/com/hishamalharrasi/byanat/ByanatBeChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.hishamalharrasi.byanat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ByanatBeChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ByanatBeChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hishamalharrasi/byanat/ByanatBeChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hishamalharrasi.byanat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ByanatBeChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
